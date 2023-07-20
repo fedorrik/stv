c1181f41ea0d09aec890d75c38d5688245f7d059
fedor <fedorrik1@gmail.com>
2023-07-20 18:16:29 +0300

    fix

--- stv.sh
+++ stv.sh
@@ -8,7 +8,7 @@ then
 fi
 
 # put here path to dir with program
-path='/home/fedor/Programs/my/stv'
+path='/home/fedor/sync/bioinformatics/centromere/soft/stv'
 
 # take live HORs only
 python3 $path/scripts/live_HORs_filter.py $1 > AS_liveHORs.bed
@@ -21,7 +21,7 @@ python3 $path/scripts/bed2stat.py stv_raw.bed > stv_stats.tsv
 python3 $path/scripts/stats2table.py stv_stats.tsv
 
 # coloring
-python3 $path/scripts/coloring.py stv_row.bed > stv_colored.bed
+python3 $path/scripts/coloring.py stv_raw.bed > stv_colored.bed
 
 # numbering
 python3 $path/scripts/numbering.py stv_colored.bed > stv.bed
